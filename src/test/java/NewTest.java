2b64ca6387544f020f6d0cd6576f4a067ccfb2cd
Kristof95 <matrixsprt@gmail.com>
2018-02-22 11:36:01 +0100

    Update NewTest.java

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,21 +1,19 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 public class NewTest {
 
     private WebDriver driver;
 
-    @BeforeTest
-    public void beforeTest() {
-        File driver = new File("/usr/bin/geckodriver");
-        FirefoxBinary ffBinary = new FirefoxBinary(driver);
-        FirefoxProfile firefoxProfile = new FirefoxProfile();
-        driver = new FirefoxDriver(ffBinary,firefoxProfile);
-    }
+
 
     @Test
     public void testEasy() {
@@ -23,9 +21,16 @@ public class NewTest {
         String title = driver.getTitle();
         Assert.assertTrue(title.contains("Demo Guru99 Page"));
     }
-    
+    @BeforeTest
+    public void beforeTest() {
+        File dirverPath = new File("/usr/bin/geckodriver");
+        FirefoxBinary ffBinary = new FirefoxBinary(dirverPath);
+        FirefoxProfile firefoxProfile = new FirefoxProfile();
+        driver = new FirefoxDriver(ffBinary,firefoxProfile);
+    }
     @AfterTest
     public void afterTest() {
         driver.quit();
     }
 }
+
