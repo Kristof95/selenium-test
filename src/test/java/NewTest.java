a167ff6a01ab88ae5fc362c13ae25dcb66a38a91
Kristóf Bodnár <Kristof.Bodnar@wup.hu>
2018-02-22 15:56:22 +0100

    add new test

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -6,21 +7,11 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.io.File;
-
 
 public class NewTest {
 
     private WebDriver driver;
 
-
-
-    @Test
-    public void testEasy() {
-        driver.get("http://demo.guru99.com/test/guru99home/");
-        String title = driver.getTitle();
-        Assert.assertTrue(title.contains("Demo Guru99 Page"));
-    }
     @BeforeTest
     public void beforeTest() {
         ChromeOptions chromeOptions = new ChromeOptions();
@@ -32,6 +23,21 @@ public class NewTest {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         driver = new ChromeDriver(chromeOptions);
     }
+
+    @Test
+    public void testEasy() {
+        driver.get("http://demo.guru99.com/test/guru99home/");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Demo Guru99 Page"));
+    }
+
+    @Test
+    public void testRtSection() {
+        driver.get("http://demo.guru99.com/test/guru99home/");
+        driver.findElement(By.id("navbar-brand-centered")).click();
+    }
+
+
     @AfterTest
     public void afterTest() {
         driver.quit();
